9aca6ebfe76f3fc1760cf713bec5b1b8ff55eafb
Artem Vorobev <v.a.vorobyov96@gmail.com>
2021-02-22 16:31:33 +0300

    js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
-const cartButton = document.querySelector("#cart-button");
-const modal = document.querySelector(".modal");
-const close = document.querySelector(".close");
-cartButton.addEventListener('click', toggleModal);
-close.addEventListener('click', toggleModal);
-function toggleModal() {
-  modal.classList.toggle("modal-open");
+'use strict';
+
+// Slider
+
+const optionSlider = {
+  sliderPerView: 1,
+  loop: true,
+  autoplay: true,
+  effect: 'coverflow',
 };
+const swiper = new Swiper('.swiper-container', optionSlider)
+
+// Modal cart
+
+const cartButton = querySelector("#cart-button");
+const modal = querySelector(".modal");
+const close = querySelector(".close");
+
+cartButton.addEventListener("click", toggleModal);
+close.addEventListener("click", toggleModal);
+
+function toggleModal() {
+  modal.classList.toggle("is-open");
+}
+
+// day 1
+
+console.log(document.querySelector('.button-auth'))
+
+
+
+init();
